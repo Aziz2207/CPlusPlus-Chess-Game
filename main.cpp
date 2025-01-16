85ad1d5190e0507e10d94285fc823ed171adffbb
Aziz2207 <133813323+Aziz2207@users.noreply.github.com>
2025-01-16 16:45:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 /*
 * Livrable 1 du projet de INF1015
 * \file main.cpp
-* \author Aziz Hidri (2171081) et Patrick Leonnel Nzudom Ketchateu (2218623)
+* \author Aziz Hidri et Patrick Leonnel Nzudom Ketchateu
 * \date 15 avril 2024
 * Cree le 09 avril 2024
 * Les images viennent de cette page wikipedia : https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
